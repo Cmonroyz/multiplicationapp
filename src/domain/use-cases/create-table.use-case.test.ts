1f5cb32b241f59e4b1b510850b7afbef17472674
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-06-10 13:42:13 +0200

    Segundo test create table.

--- src/domain/use-cases/create-table.use-case.test.ts
+++ src/domain/use-cases/create-table.use-case.test.ts
@@ -10,4 +10,19 @@ describe( 'CreateTableUseCase', () => {
     expect ( table ).toContain( '2 x 1 = 2' );
     expect ( rows).toBe( 10 );
   });
+
+  test ('should create table with custom values', () => {
+    const options = { 
+      base: 3,
+      limit: 6
+    };
+    //console.log( 'options', options );
+    
+    const createTable = new CreateTable();
+    const table = createTable.execute( options );
+    const rows = table.split( '\n' ).length;
+    //console.log(table);
+    expect( table ).toContain( '3 x 1 = 3' );
+    expect(rows).toBe( options.limit );
+  });
 });    
