f2515628df2e1a82eaf41ddd3b087e65644adbf5
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-06-10 14:29:38 +0200

    Test: agregar test de save-file con custom options.

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -5,16 +5,29 @@ import { SaveFile } from './save-file.use-case';
 describe('SaveFileUseCase', () => {
   // borrar el direcctorio antes de las pruebas
   beforeEach(() => {
-    fs.rmSync('outputs', { recursive: true });
+    const filePath = [
+      'outputs',
+      'custom_outputs',
+    ]
+    filePath.forEach(path => {
+      if (fs.existsSync(path)) {
+        fs.rmSync(path, { recursive: true });
+      }
+    });
   });
 
   afterEach(() => {
-    // borrar el directorio después de las pruebas
-    if (fs.existsSync('outputs')) {
-      fs.rmSync('outputs', { recursive: true });
-    }
+      const filePath = [
+      'outputs',
+      'custom_outputs',
+    ]
+    filePath.forEach(path => {
+      if (fs.existsSync(path)) {
+        fs.rmSync(path, { recursive: true });
+      }
+    });
   });
-  
+
   test('should create file with default values', () => {
     const saveFile = new SaveFile();
     const filePath = 'outputs/table.txt';
@@ -29,6 +42,23 @@ describe('SaveFileUseCase', () => {
     expect(result).toBe(true);
     expect(fileExists).toBe(true);
     expect(fileContent).toBe( options.fileContent );
+  });
+
+  test('should save file wirh custom values', () => {
+    const options = {
+      fileContent: 'custom content',
+      fileDestination: 'custom_outputs/file-destination',
+      fileName: 'custom-table-name',
+    }
+    const saveFile = new SaveFile();
+    const result = saveFile.execute(options);
+    const filePath = `${options.fileDestination}/${options.fileName}.txt`;
+    const fileExists = fs.existsSync(filePath);
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8'});
+
+    expect(result).toBe(true);
+    expect(fileExists).toBe(true);
+    expect(fileContent).toBe(options.fileContent);
   });
 
 });
